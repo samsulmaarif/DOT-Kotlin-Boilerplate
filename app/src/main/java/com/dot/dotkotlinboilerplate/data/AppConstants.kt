84fd886b9290faa7c76d5444e18da912960687df
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-07-09 16:20:03 +0700

    :pencil: bind animation

--- app/src/main/java/com/dot/dotkotlinboilerplate/data/AppConstants.kt
+++ app/src/main/java/com/dot/dotkotlinboilerplate/data/AppConstants.kt
@@ -5,5 +5,6 @@ object AppConstants {
     const val TAG_DEBUG = "result_debug"
     const val TAG_ERROR = "result_error"
     const val CONNECTION_TIME_OUT = 30 * 1000L
+    const val GLOBAL_ANIM_DURATION = 500L
 
 }
--- app/src/main/java/com/dot/dotkotlinboilerplate/utils/binding/Converters.kt
+++ app/src/main/java/com/dot/dotkotlinboilerplate/utils/binding/Converters.kt
@@ -1,8 +1,15 @@
 package com.dot.dotkotlinboilerplate.utils.binding
 
+import android.animation.Animator
 import android.databinding.BindingAdapter
 import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.widget.ProgressBar
 import com.bumptech.glide.GenericTransitionOptions
+import com.dot.dotkotlinboilerplate.data.AppConstants
 
 import com.dot.dotkotlinboilerplate.utils.glide.GlideApp
 
@@ -17,6 +24,45 @@ class Converters {
                     .transition(GenericTransitionOptions.with(android.R.anim.fade_in))
                     .into(imageView)
         }
+
+        @JvmStatic
+        @BindingAdapter("bind:showRecycler")
+        fun showRecycler(recyclerView: RecyclerView, hide: Boolean){
+            if(hide) {
+                recyclerView.alpha = 0f
+                recyclerView.animate().alpha(1f).duration = AppConstants.GLOBAL_ANIM_DURATION
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("bind:hideProgressBar")
+        fun hideProgressBar(progressBar: ProgressBar, hide: Boolean){
+            if (hide) {
+                progressBar.visibility = View.VISIBLE
+                progressBar.alpha = 1f
+            } else {
+                progressBar.animate().alpha(0f).setDuration(AppConstants.GLOBAL_ANIM_DURATION).setListener(object : Animator.AnimatorListener {
+
+                    override fun onAnimationRepeat(p0: Animator?) {
+
+                    }
+
+                    override fun onAnimationEnd(p0: Animator?) {
+                        progressBar.visibility = View.INVISIBLE
+                    }
+
+                    override fun onAnimationStart(p0: Animator?) {
+
+                    }
+
+                    override fun onAnimationCancel(p0: Animator?) {
+
+                    }
+
+                })
+            }
+        }
+
     }
 
 }
