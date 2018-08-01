a1109e44237cf147b2f36d367a4e21e25a86e24f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-08-01 09:26:09 +0700

    :pencil: remove Rx Kotlin

--- app/src/main/java/com/dot/dotkotlinboilerplate/menu/listplace/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/dot/dotkotlinboilerplate/menu/listplace/viewmodels/MainViewModel.kt
@@ -4,14 +4,11 @@ import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import android.databinding.ObservableField
-import android.util.Log
-import com.dot.dotkotlinboilerplate.data.AppConstants
 import com.dot.dotkotlinboilerplate.menu.listplace.models.ListPlaceModel
 import com.dot.dotkotlinboilerplate.networks.ListPlaceResponse
 
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
@@ -26,21 +23,18 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
         isLoading.set(true)
         compositeDisposable.add(
             listPlaceResponse.getListPlace()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                        onNext = {
-                            Log.d(AppConstants.TAG_DEBUG,"MainViewModel # data size ${it.data?.size}")
-                            listData.value = it
-                        },
-                        onError = {
-                            it.printStackTrace()
-                            Log.e(AppConstants.TAG_ERROR,"MainViewModel # error requestListPlace ${it.message}")
-                        },
-                        onComplete = {
-                            isLoading.set(false)
-                        }
-                )
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            {
+                                isLoading.set(false)
+                                listData.value = it
+                            },
+                            {
+                                isLoading.set(false)
+                                it.printStackTrace()
+                            }
+                    )
         )
     }
 
